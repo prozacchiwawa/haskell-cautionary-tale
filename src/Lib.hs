d1dd4e0fcd95fda0ddbbe6432ab77833c93c37f6
arty <art.yerkes@gmail.com>
2019-12-23 08:56:36 -0800

    First step, things are normal

--- src/Lib.hs
+++ src/Lib.hs
@@ -2,5 +2,27 @@ module Lib
     ( someFunc
     ) where
 
+import Prelude
+import Data.Text
+
+data Config =
+  Config
+    { theFilename :: Text
+    }
+
+doSomeWork :: Config -> IO ()
+doSomeWork cfg@Config {..} = do
+  putStrLn $ "The filename is " ++ (unpack theFilename)
+
+  doMoreWork cfg
+
+doMoreWork :: Config -> IO ()
+doMoreWork Config {..} = do
+  putStrLn $ "More work: " ++ (unpack theFilename)
+
 someFunc :: IO ()
-someFunc = putStrLn "someFunc"
+someFunc = do
+  let
+    config = Config "foo.txt"
+
+  doSomeWork config
